a9232d0f93db76c1a20584cad4c14ec90841d3d4
Rafael Rodriguez <rafadjrr@gmail.com>
2022-09-06 16:28:36 -0400

    practica con list views y busqueda por kwords

--- apps/empleado/urls.py
+++ apps/empleado/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('listar_todo_empleados/',views.EmpleadoListView.as_view()),
     path('listar_ventas/<departamento>/',views.AreaEmpleadoListView.as_view()),
+    path('listar_jobs/<jobs>/',views.EmpleadoxJobsListView.as_view()),
+    path('buscar_empleado/<jobs>/',views.EmpleadosByKwordListView.as_view())
 ]
--- apps/empleado/views.py
+++ apps/empleado/views.py
@@ -19,3 +19,25 @@ class AreaEmpleadoListView(ListView):
         depto = self.kwargs['departamento']        
         lista = Empleado.objects.filter(departamento__name=depto)
         return lista
+
+class EmpleadoxJobsListView(ListView):
+    #model = Empleado
+    template_name = 'empleado/jobs_emple.html'
+    
+
+    def get_queryset(self):
+        pjobs = self.kwargs['jobs']        
+        lista = Empleado.objects.filter(jobs=pjobs)
+        return lista
+
+
+class EmpleadosByKwordListView(ListView):
+    model = Empleado
+    template_name = "empleado/by_kword.html"
+    context_object_name = "empleados"
+    def get_queryset(self):
+        print('=====================')
+        palabra_clave = self.request.GET.get("kword",'')
+        lista = Empleado.objects.filter(first_name=palabra_clave)
+        print(lista)
+        return lista
