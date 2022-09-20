b146de31c3637eb1e0a796fd76d4873ec72839b4
Rafael Rodriguez <rafadjrr@gmail.com>
2022-09-20 15:26:18 -0400

    structure static file added

--- nomina/settings/base.py
+++ nomina/settings/base.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [BASE_DIR.child('static')]
+STATICFILES_DIRS = [BASE_DIR.child('static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
