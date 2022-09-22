3230af5e68cfd90f7fd59d5274b02fdf9aa0629d
Rafael Rodriguez <rafadjrr@gmail.com>
2022-09-22 16:05:16 -0400

    update name url root from empleados

--- nomina/urls.py
+++ nomina/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('hm/', include('apps.home.urls')),
-    path('empleado/', include('apps.empleado.urls')),
+    path('nomina/', include('apps.empleado.urls')),
     path('dpto/', include('apps.departamento.urls')),
     
 ]
