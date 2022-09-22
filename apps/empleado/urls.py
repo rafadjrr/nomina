9383d5723a807f233389365ba5353b34e64d81ca
Rafael Rodriguez <rafadjrr@gmail.com>
2022-09-22 15:33:41 -0400

    add action view empleados in list empleados

--- apps/empleado/urls.py
+++ apps/empleado/urls.py
@@ -6,7 +6,7 @@ app_name = "empleado_app"
 urlpatterns = [
     path('',views.InicioView.as_view(),name='inicio'),
     path('listar_empleados/',views.EmpleadoListView.as_view(),name='empleados_all'),
-    path('listar_ventas/<departamento>/',views.AreaEmpleadoListView.as_view()),
+    path('listar_area_emple/<departamento>/',views.AreaEmpleadoListView.as_view(),name='empleados_area'),
     path('listar_jobs/<jobs>/',views.EmpleadoxJobsListView.as_view()),
     path('buscar_empleado/',views.EmpleadosByKwordListView.as_view()),
     path('buscar_empleado_x_habilidades/<id>/',views.HabilidadesEmpleadosListView.as_view()),
